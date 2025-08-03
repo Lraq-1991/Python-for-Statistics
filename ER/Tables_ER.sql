e595d1492464d2875c96b201b576a45ff83b02ce
Lraq-1991 <alvarezquinteroluis@gmail.com>
2025-08-03 16:55:58 -0300

    Update Tables_ER.sql

--- ER/Tables_ER.sql
+++ ER/Tables_ER.sql
@@ -1,78 +1,83 @@
 CREATE TABLE "DimEmployees" (
-  "employee_id" integer PRIMARY KEY,
-  "name" varchar,
-  "email" varchar,
+  "employee_id" integer SERIAL PRIMARY KEY,
+  "name" varchar NOT NULL,
+  "email" varchar NOT NULL,
   "salary" varchar,
-  "entry" datetime,
+  "entry" date NOT NULL,
   "role_id" integer,
   "status" boolean
 );
 
 CREATE TABLE "DimDepartments" (
-  "department_id" integer PRIMARY KEY,
-  "name" varchar,
-  "created_at" timestamp
+  "department_id" integer SERIAL PRIMARY KEY,
+  "name" varchar NOT NULL,
+  "created_at" timestamp NOT NULL
 );
 
 CREATE TABLE "DimRoles" (
-  "role_id" integer PRIMARY KEY,
-  "department_id" integer,
+  "role_id" integer SERIAL PRIMARY KEY,
+  "department_id" integer NOT NULL,
   "title" varchar,
   "description" text,
-  "created_at" timestamp,
-  "status" boolean
+  "created_at" timestamp NOT NULL,
+  "status" boolean NOT NULL
 );
 
 CREATE TABLE "FactJob_history" (
-  "job_id" integer PRIMARY KEY,
-  "employee_id" integer,
+  "job_id" integer SERIAL PRIMARY KEY,
+  "employee_id" integer NOT NULL,
   "salary" float,
-  "entry" datetime,
-  "end_date" datetime,
+  "entry" date,
+  "end_date" date,
   "duration" integer
 );
 
 CREATE TABLE "DimProducts" (
-  "product_id" integer PRIMARY KEY,
-  "name" varchar,
+  "product_id" integer SERIAL PRIMARY KEY,
+  "name" varchar NOT NULL,
   "description" text,
   "quantity" integer,
   "price" float,
   "category" text,
-  "supplier_id" integer
+  "supplier_id" integer NOT NULL
 );
 
 CREATE TABLE "DimCustomers" (
-  "customer_id" integer PRIMARY KEY,
+  "customer_id" integer SERIAL PRIMARY KEY,
   "name" varchar,
   "address" varchar,
-  "phone" varchar
+  "phone" varchar NOT NULL
 );
 
 CREATE TABLE "DimSuppliers" (
-  "supplier_id" integer PRIMARY KEY,
+  "supplier_id" integer SERIAL PRIMARY KEY,
   "supplier_name" varchar,
-  "contact_person" varchar,
-  "email" varchar
+  "contact_person" varchar NOT NULL,
+  "email" varchar NOT NULL
 );
 
 CREATE TABLE "FactOrders" (
-  "order_id" integer PRIMARY KEY,
-  "customer_id" integer,
-  "employee_id" integer,
+  "order_id" integer SERIAL PRIMARY KEY,
+  "customer_id" integer NOT NULL,
+  "employee_id" integer NOT NULL,
   "products" integer[],
   "order_time" timestamp,
-  "delivery_date" datetime,
+  "delivery_date" date,
   "total_amount" float,
   "tax_amount" float,
   "revenue" float
 );
 
 CREATE TABLE "DimShipments" (
-  "shipment_id" integer PRIMARY KEY,
-  "order_id" integer,
+  "shipment_id" integer SERIAL PRIMARY KEY,
+  "order_id" integer NOT NULL,
   "shipment_date" datetime,
-  "status" varchar
+  "status_id" integer NOT NULL
+);
+
+CREATE TABLE "DimStatuses" (
+	"status_id" integer SERIAL PRIMARY KEY,
+	"name" varchar NOT NULL
 );
 
 COMMENT ON COLUMN "DimRoles"."description" IS 'Specification of the role';
@@ -96,3 +101,6 @@ ALTER TABLE "DimProducts" ADD FOREIGN KEY ("product_id") REFERENCES "FactOrders"
 ALTER TABLE "DimProducts" ADD FOREIGN KEY ("supplier_id") REFERENCES "DimSuppliers" ("supplier_id");
 
 ALTER TABLE "FactOrders" ADD FOREIGN KEY ("order_id") REFERENCES "DimShipments" ("order_id");
+
+ALTER TABLE "DimShipments" ADD FOREIGN KEY ("status_id") REFERENCES "DimStatuses" ("status_id");
+
